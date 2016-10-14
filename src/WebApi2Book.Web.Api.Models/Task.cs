b1eb5e792ad5954448a2244ba5c98dd39fda071c
dinkpopo <yeyu_fox@163.com>
2016-10-14 13:48:14 +0800

    update task

--- src/WebApi2Book.Web.Api.Models/Task.cs
+++ src/WebApi2Book.Web.Api.Models/Task.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi2Book.Web.Api.Models
 {
-    public class Task: ILinkContaining
+    public class Task : ILinkContaining
     {
         private List<Link> _links;
+        private bool _shouldSerializeAssignees;
 
+        [Key]
         public long? TaskId { get; set; }
+
+        [Editable(true)]
         public string Subject { get; set; }
+
+        [Editable(true)]
         public DateTime? StartDate { get; set; }
+
+        [Editable(true)]
         public DateTime? DueDate { get; set; }
+
+        [Editable(false)]
         public DateTime? CreatedDate { get; set; }
+
+        [Editable(false)]
         public DateTime? CompletedDate { get; set; }
+
+        [Editable(false)]
         public Status Status { get; set; }
+
+        [Editable(false)]
         public List<User> Assignees { get; set; }
+
+        [Editable(false)]
         public List<Link> Links
         {
             get { return _links ?? (_links = new List<Link>()); }
@@ -25,8 +44,6 @@ namespace WebApi2Book.Web.Api.Models
         {
             Links.Add(link);
         }
-
-        private bool _shouldSerializeAssignees;
 
         public void SetShouldSerializeAssignees(bool shouldSerialize)
         {
